dc1becf2eaacdcaf51593776d72cc58bd4eadb39
Rob <cdrh41@gmail.com>
2018-12-09 22:07:27 -0500

    v2

--- liri.js
+++ liri.js
@@ -3,30 +3,91 @@ require('dotenv').config();
 const keys = require('./keys.js');
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
-//const omdb = new Omdb(keys.omdb);
-//const movieSource = `http://www.omdbapi.com/?t=${movieName}&apikey=trilogy`;
 const request = require('request');
 const action = process.argv[2];
 let searchInfo = process.argv.slice(3).join(' ');
 const artist = searchInfo;
+const song = searchInfo;
+const movieName = searchInfo;
+
+const findArtist = function(artist){
+
+
 const URL = `https://rest.bandsintown.com/artists/${artist}/events/?app_id=codingbootcamp`;
     request(URL, function (err, response, body) {
     if (err) {
-//  return console.log('Error: ${err}');
+  return console.log('Error: ${err}');
 }
 else {
+
   const dataList = JSON.parse(body);
-  console.log('==============================================');
-  console.log(`Artist Name ${searchInfo}`);
-  console.log('==============================================');
+
+console.log('==============================================');
+console.log(`Artist Name ${searchInfo}`);
+
+for (i=0; i < dataList.length; i++) {
+
+  console.log(dataList[i].venue.name);
+
+}
+
+console.log('==============================================');
+};
+
+});
+}
+const movieThis = function(movie){
+
+
+const movieSource = `http://www.omdbapi.com/?t=${movieName}&y=&plot=full&tomatoes=true
+&apikey=trilogy`;
+request(movieSource, function (err, response, body) {
+  if (err) {
+  return console.log('Error: ${err}');
+}
+else {
+  const movieList = JSON.parse(body);
+  console.log(movieList.Title);
+  console.log(movieList.Year);
+  console.log(movieList.imdbRating);
+  if (movieList.Ratings[1] == null){
+    console.log('no tomatoes!');
+  } 
+  else{
+    console.log(movieList.Ratings[1].Value);
+  }
+  
+  console.log(movieList.Country);
+  console.log(movieList.Language);
+  console.log(movieList.Plot);
+  console.log(movieList.Actors);
 }
 });
+}
+const spotifyThis = function(song){
+  spotify.search({ type: 'track', query: song }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
+}
 
-/*switch (action) {
+
+switch (action) {
   case 'concert-this':
-    concertThis();
+    findArtist();
     break
-}*/
+
+    case 'spotify-this-song':
+    spotifyThis(song);
+    break
+
+    case 'movie-this':
+    movieThis();
+    break
+}
  
 
 /*omdb.search({ type: 'type', query: 'What Matters' }, function(err, data) {
